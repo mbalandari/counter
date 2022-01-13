caa5e252728622445df3e8236caecccdb8ae94ce
mbalandari <m.balandari.toroghi@gmail.com>
2022-01-13 11:09:35 +0300

    add scale

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const TodayData = [20, 30, 50, 6, 4, 2, 5, 20, 24, 32, 40, 59, 68, 89, 100, 112, 109, 101, 78, 56, 32, 31, 12, 1];
-
 const todaySvg = d3.select("svg");
+
+const barScale = d3.scaleLinear()
+    .domain([0, 2000])
+    .range([1, 112])
 
 todaySvg
     .selectAll("rect")
-    .data(TodayData)
+    .data(todayData)
     .enter()
     .append("rect")
-    .attr("width", (d, i) => { return d })
-    .attr("y", (d, i) => { return 20 * i })
-    .attr("x", 0)
-    .attr("height", 16)
+    .attr("x", (d, i) => { return i * 36 })
+    .attr("y", (d, i) => { return 112 - barScale(d) })
+    .attr("width", 24)
+    .attr("height", (d, i) => { return barScale(d) })
