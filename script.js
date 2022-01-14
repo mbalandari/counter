9556559aa002ad52a75be6524fd40738e71e39aa
mbalandari <m.balandari.toroghi@gmail.com>
2022-01-14 10:20:23 +0300

    add transition

--- script.js
+++ script.js
@@ -32,10 +32,6 @@ const raduisScale = d3.scaleSqrt()
     .domain([0, 30000])
     .range([0, 50])
 
-const colorScale = d3.scaleSqrt()
-    .domain([0, 30000])
-    .range(["blue", "red"])
-
 monthSvg
     .selectAll("circle")
     .data(monthData)
@@ -43,5 +39,7 @@ monthSvg
     .append("circle")
     .attr("cx", (d, i) => { return (i % 7) * 120 + 60 })
     .attr("cy", (d, i) => { return Math.floor(i / 7) * 100 + 60 })
+    .attr("r", 0)
+    .transition()
+    .duration(1000)
     .attr("r", (d, i) => { return raduisScale(d) })
-    .attr("fill", (d, i) => { return colorScale(d) })
