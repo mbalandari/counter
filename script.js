7c467fd827808e53cd4c7532a507370ef43fbb82
mbalandari <m.balandari.toroghi@gmail.com>
2022-01-14 11:24:33 +0300

    group bar chart

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ const barScale = d3.scaleLinear()
     .domain([0, 2000])
     .range([1, 112])
 
-todaySvg
-    .selectAll("rect")
+const todayGroups = todaySvg
+    .selectAll("g")
     .data(todayData)
     .enter()
+    .append("g")
+    .attr("transform", (d, i) => { return "translate(" + (i * 36) + ", 0)" })
+
+todayGroups
     .append("rect")
-    .attr("x", (d, i) => { return i * 36 })
+    .attr("x", 0)
     .attr("y", (d, i) => { return 112 })
     .attr("width", 24)
     .attr("height", 0)
@@ -19,12 +23,9 @@ todaySvg
     .attr("y", (d, i) => { return 112 - barScale(d) })
     .attr("height", (d, i) => { return barScale(d) })
 
-todaySvg
-    .selectAll("text")
-    .data(todayData)
-    .enter()
+todayGroups
     .append("text")
-    .attr("x", (d, i) => { return i * 36 + 12 })
+    .attr("x", 12)
     .attr("y", 130)
     .text((d, i) => { return i })
 
